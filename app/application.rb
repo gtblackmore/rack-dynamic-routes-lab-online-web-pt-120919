353ed7c2dc6ccef608baec6edd62eab3dbbc286f
George Blackmore <gtblackmore@gmail.com>
2020-03-07 23:38:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,6 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
       if item =@@items.find{|i| i.name == item_name}
         resp.write item.price
       else 
