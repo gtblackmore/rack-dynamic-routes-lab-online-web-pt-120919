9ffa44f7ddd9a2b91967bb38560dbe23b7150b89
George Blackmore <gtblackmore@gmail.com>
2020-03-07 23:40:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,8 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
       else 
         resp.status = 400
         resp.write "Item not found"
